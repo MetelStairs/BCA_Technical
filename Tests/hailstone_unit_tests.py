87a3fdf14c9c228d9e661fdae0908d0b2592e597
MetelStairs <47181804+MetelStairs@users.noreply.github.com>
2023-12-09 18:32:25 +0000

    testing push
    Half tests done

--- Tests/hailstone_unit_tests.py
+++ Tests/hailstone_unit_tests.py
@@ -1,4 +1,68 @@
-import hailstone
+import unittest
+from hailstone import HailstoneClass
 
 
-hailstone.main_hailstone_function("car")
+class TestHailstone(unittest.TestCase):
+    def test_non_int_input(self):
+        # Test with an invalid starting number (non-integer)
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("car")
+            print("String input passed")
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("22.3")
+            print("Float input passed")
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("")
+            print("Empty input passed")
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence(True)
+            print("Boolean input passed")
+        print("Non Integer inputs have successfully passed")
+
+    def test_less_than_1(self):
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("1")
+            print("1 input passed")
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("0")
+            print("0 input passed")
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence("-1")
+            print("-1 input passed")
+        print("Less than 1 input input successfully passed")
+
+    def test_correct_answers(self):
+        sequence, steps, text_summary = HailstoneClass.main_hailstone_function(14)
+        print(sequence)
+        print(steps)
+        self.assertEqual(sequence, [14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(steps, 17)
+        print("Input of 14 has passed")
+
+        sequence, steps = HailstoneClass.main_hailstone_function(25)
+        self.assertEqual(sequence, [25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(steps, 23)
+        print("Input of 25 has passed")
+
+
+        """
+    def test_hailstone_sequence(self):
+        # Test with a valid starting number
+        sequence, steps, text_summary = HailstoneClass.hailstone_sequence(6)
+        self.assertEqual(sequence, [6, 3, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(steps, 8)
+        self.assertIsInstance(text_summary, str)
+
+        # Test with an invalid starting number (non-integer)
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence(3)
+
+        # Test with an invalid starting number (equal to 1)
+        with self.assertRaises(ValueError):
+            HailstoneClass.hailstone_sequence(1)
+        """
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- hailstone.py
+++ hailstone.py
@@ -1,64 +1,70 @@
+class HailstoneClass:
+    def __init__(self, starting_number):
+        self.starting_number = starting_number
+        self.steps = 0
+        self.sequence = []
+        self.text_summary
 
-def is_integer(value):
-    """
-    Function for checking if inputted value is Int
+    def is_integer(value):
+        """
+        Function for checking if inputted value is Int
+        """
+        try:
+            return type(int(value)) == int
 
-    """
-    try:
-        int(value)
-        return True
-    except ValueError:
-        return False
+        except ValueError:
+            return False
+
+    def hailstone_sequence(starting_number):
+        """
+        Function that does the hailstone mathematical compute
+        """
+        if not HailstoneClass.is_integer(starting_number):
+            raise ValueError("The starting number must be an integer")
+
+        # Ensures the variable is an Int after checking
+        starting_number = int(starting_number)
+
+        if starting_number <= 1:
+            raise ValueError("The starting number must be greater than 1")
+
+        # Initialize the empty variables for tracking steps
+        steps = 0
+        sequence = []
+        # Make the starting number the first entry in the sequence
+        orginal_starting_number = starting_number
+        sequence = [starting_number]
+
+        while starting_number != 1:  # While the number is not 1
+            if starting_number % 2 == 0:  # If the number is even then divide by 2
+                starting_number = starting_number // 2
+            else:  # Otherwise if its an odd number then multiply by 3 and add 1
+                starting_number = 3 * starting_number + 1
+
+            steps = steps + 1  # Add +1 for each loop done
+            sequence.append(starting_number)  # Append each number to a list for the full sequence
+            # Write a brief summary (I think this is what was wanted by Textual Summary)
+            text_summary = f"The starting number of {orginal_starting_number} reaches 1 in {steps} steps"
+
+        return sequence, steps, text_summary
 
 
-def hailstone_sequence(starting_number):
-    """
-    Function that does the hailstone mathematic compute
-    """
-    if not is_integer(starting_number):
-        raise ValueError("The starting number must be an integer")
+    def main_hailstone_function(starting_number):
+        """
+        Function prints the output and potential errors of the hailstone sequence
+        """
+        try:
+            sequence, steps, text_summary = HailstoneClass.hailstone_sequence(starting_number)
 
-    # Ensures the variable is an Int after checking
-    starting_number = int(starting_number)
+            print(f"Starting Number: {starting_number}")
+            print(f"Number of steps: {steps}")
+            print(f"Sequence: {sequence}")
+            print(text_summary)
 
-    if starting_number == 1:
-        raise ValueError("The starting number cannot be 1")
-
-    # Initialize the empty variables for tracking steps
-    steps = 0
-    sequence = []
-    # Make the starting number the first entry in the sequence
-    sequence = [starting_number]
-
-    while starting_number != 1:  # While the number is not 1
-        if starting_number % 2 == 0:  # If the number is even then divide by 2
-            starting_number = starting_number // 2
-        else:  # Otherwise if its an odd number then multiply by 3 and add 1
-            starting_number = 3 * starting_number + 1
-
-        steps = steps + 1  # Add +1 for each loop done
-        sequence.append(starting_number)  # Append each number to a list for the full sequence
-        text_summary = f"The starting number of {starting_number} reaches 1 in {steps} steps"
-
-    return sequence, steps, text_summary
-
-
-def main_hailstone_function(starting_number):
-    """
-    Function prints the output and potential errors of the hailstone sequence
-    """
-    try:
-        sequence, steps, text_summary = hailstone_sequence(starting_number)
-
-        print(f"Starting Number: {starting_number}")
-        print(f"Number of steps: {steps}")
-        print(f"Sequence: {sequence}")
-        print(text_summary)
-
-    except ValueError as e:
-        print(e)
+        except ValueError as e:
+            print(e)
 
 
 if __name__ == "__main__":
-    start_number = "1"
-    main_hailstone_function(start_number)
+    starting_number = 25
+    HailstoneClass.main_hailstone_function(starting_number)
