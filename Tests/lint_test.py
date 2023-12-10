c658c2fb6ffd7216a4d4c34e727cd212bd2b5dd8
MetelStairs <47181804+MetelStairs@users.noreply.github.com>
2023-12-10 15:37:26 +0000

    lint test added

--- Tests/lint_test.py
+++ Tests/lint_test.py
@@ -4,11 +4,14 @@ import os
 
 def lint_function():
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    file_to_test = os.path.join(base_dir, "hailstone.py")
+    file_to_test = os.path.join(base_dir, "hailstone.py")  # Pointing to the correct file for linting
 
-    result = subprocess.run(['flake8', file_to_test], capture_output=True, text=True)
+    # Run linting on the file using the .flake8 file in the test folder
+    result = subprocess.run(['flake8', '--config=.flake8', file_to_test], capture_output=True, text=True)
+    # Fail the function if the linting is not correct
     assert result.returncode == 0, f"Linting failed:\n{result.stdout}"
 
 
 if __name__ == '__main__':
     lint_function()
+    print("Lint Test Passed")
