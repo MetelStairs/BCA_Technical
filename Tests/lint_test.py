516949cdfce5958b6da0036bccdce7d16c744f4e
MetelStairs <47181804+MetelStairs@users.noreply.github.com>
2023-12-10 15:40:34 +0000

    Update lint_test.py

--- Tests/lint_test.py
+++ Tests/lint_test.py
@@ -7,7 +7,7 @@ def lint_function():
     file_to_test = os.path.join(base_dir, "hailstone.py")  # Pointing to the correct file for linting
 
     # Run linting on the file using the .flake8 file in the test folder
-    result = subprocess.run(['flake8', '--config=.flake8', file_to_test], capture_output=True, text=True)
+    result = subprocess.run(['flake8', '--config=Tests/.flake8', file_to_test], capture_output=True, text=True)
     # Fail the function if the linting is not correct
     assert result.returncode == 0, f"Linting failed:\n{result.stdout}"
 
